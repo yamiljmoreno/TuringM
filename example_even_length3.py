7996fc03e96d7656bfe5521a56782d71e8b9e33e
Yamil Josue Moreno <114109636+yamiljmoreno@users.noreply.github.com>
2022-11-22 02:15:48 -0600

    Add files via upload

---
+++ example_even_length3.py
@@ -0,0 +1,18 @@
+from tmsim import *
+
+value = input('Enter a Palindrome:\n')
+
+Algorithm({
+    'q_even': {
+        '0': ('q_odd', '->'),
+        '1': ('q_odd', '->'),
+        '[]': True,
+    },
+    'q_odd': {
+        '0': ('q_even', '->'),
+        '1': ('q_even', '->'),
+        '[]': False,
+    }
+}, initial_state='q_even').test({type(value)}, 10),
+    lambda word: len(word) % 2 == 0
+)
